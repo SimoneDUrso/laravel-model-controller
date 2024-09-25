4a6a1bb095cae3841814ce1658b6c2ea8c0df156
SimoneDUrso <simone.durso98@gmail.com>
2024-09-25 16:20:21 +0200

    [UPD] Restyle the pages. Exercise completed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center mt-5 pt-5 text-dark">Benvenuto sul mio sito!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center mt-5 pt-5 text-dark">Benvenuto sul mio sito!</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <h2 class="text-center">Clicca su Movies se ti va👉👈</h2>
+            <div class="gif">
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-12">
                 <ul class="list-unstyled d-flex fs-4 justify-content-center mt-3">
-                    <li><a class="text-decoration-none text-white" href="{{ route('home') }}">Home</a></li>
-                    <li class="mx-5"><a class="text-decoration-none text-white " href="{{ route('movie') }}">Movies</a>
+                    <li><a class="text-decoration-none" href="{{ route('home') }}">Home</a></li>
+                    <li class="mx-5"><a class="text-decoration-none " href="{{ route('movie') }}">Movies</a>
                     </li>
                 </ul>
             </div>
